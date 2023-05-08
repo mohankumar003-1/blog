f8cb582402051ff88ed40aa29f2e1a432188c1c1
mohankumar003-1 <115139042+mohankumar003-1@users.noreply.github.com>
2023-05-08 17:28:22 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,11 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
-)
+STATIC_ROOT = 'mohankumar003-1/blog/static/css/blog/css/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
